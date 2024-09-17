bcbe31d526dd115fc16fb385bf0cce67821622f0
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-09-17 13:18:47 +0200

    Aggiunto axios e creato chiamata + stampato in pagina risultati in lista

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,14 @@
 */
 // 1) Importazione del componente
 
+import axios from 'axios';
+
 export default {
   data() {
     return {
+      apiKey: 'dcdaddac05c607760f32e817be2c0842',
+      searchText: '',
+      movies: []
     }
   },
   // 2) Dichiarazione del componente
@@ -20,6 +25,18 @@ export default {
     search() {
       console.log(this.searchText);
       
+      axios
+        .get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+            api_key: this.apiKey,
+            query: this.searchText,
+          }
+        })
+        .then((resp) => {
+          console.log(resp.data);
+
+          this.movies = resp.data.results;
+        });
     }
   }
 }
@@ -30,7 +47,7 @@ export default {
     <div class="d-flex justify-content-between">
     <div>
       <form @submit.prevent="search">
-        <input type="text" placeholder="Cerca film o serie TV...">
+        <input v-model="searchText" type="text" placeholder="Cerca film o serie TV...">
         <button type="submit">
           Cerca
         </button>
@@ -38,11 +55,35 @@ export default {
     </div>
 
     <div>
-      <input type="text" placeholder="Cerca film o serie TV...">
+      <input v-model="searchText" type="text" placeholder="Cerca film o serie TV...">
       <button @click="search">
           Cerca
       </button>
     </div>
+  </div>
+
+  <div>
+    <ol>
+      <li v-for="(movie ,i) in movies" :key="i">
+        <ul>
+          <li>
+            Titolo: {{ movie.title }}
+          </li>
+          <li>
+            Titolo Originale: {{ movie.original_title }}
+          </li>
+          <li>
+            Lingua: {{ movie.original_language }}
+          </li>
+          <li>
+            Voto: {{ movie.vote_average }}
+          </li>
+        </ul>
+
+        <hr>
+
+      </li>
+    </ol>
   </div>
 </template>
 
