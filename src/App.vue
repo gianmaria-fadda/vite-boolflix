3fc535907a04fd94ebeee87e46ef5b5b456d25bd
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-09-17 13:57:35 +0200

    Completata milestone 2 dentro App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,8 @@ export default {
     return {
       apiKey: 'dcdaddac05c607760f32e817be2c0842',
       searchText: '',
-      movies: []
+      movies: [],
+      series: []
     }
   },
   // 2) Dichiarazione del componente
@@ -33,10 +34,36 @@ export default {
           }
         })
         .then((resp) => {
-          console.log(resp.data);
+          console.log('Movies', resp.data);
 
           this.movies = resp.data.results;
         });
+
+        axios
+        .get('https://api.themoviedb.org/3/search/tv', {
+          params: {
+            api_key: this.apiKey,
+            query: this.searchText,
+          }
+        })
+        .then((resp) => {
+          console.log('Series', resp.data);
+
+          this.series = resp.data.results;
+        });
+    },
+    getFlag(lang) {
+      const validLang = {
+        en: 'en.gif',
+        it: 'it.gif',
+        ja: 'jp.gif',
+      };
+
+      if (lang in validLang) {
+        return '/img/flags/' + validLang[lang];
+      } else {
+        return '/img/flags/other-img.webp';
+      }
     }
   }
 }
@@ -63,6 +90,9 @@ export default {
   </div>
 
   <div>
+    <h2>
+      Movies
+    </h2>
     <ol>
       <li v-for="(movie ,i) in movies" :key="i">
         <ul>
@@ -73,10 +103,39 @@ export default {
             Titolo Originale: {{ movie.original_title }}
           </li>
           <li>
-            Lingua: {{ movie.original_language }}
+            Lingua: <img :src="getFlag(movie.original_language)" alt="">
           </li>
           <li>
             Voto: {{ movie.vote_average }}
+          </li>
+        </ul>
+
+        <hr>
+
+      </li>
+    </ol>
+  </div>
+
+  <hr>
+
+  <div>
+    <h2>
+      Series
+    </h2>
+    <ol>
+      <li v-for="(series ,i) in series" :key="i">
+        <ul>
+          <li>
+            Titolo: {{ series.name }}
+          </li>
+          <li>
+            Titolo Originale: {{ series.original_title }}
+          </li>
+          <li>
+            Lingua: <img :src="getFlag(series.original_language)" alt="">
+          </li>
+          <li>
+            Voto: {{ series.vote_average }}
           </li>
         </ul>
 
@@ -91,4 +150,8 @@ export default {
 @use 'assets/scss/main' as *;
 // Import all of Bootstrap's CSS
 @import "bootstrap/scss/bootstrap";
+
+img {
+  max-width: 25px;
+}
 </style>
